78be06027390697b0c71c99272e61cd1e27c0864
ShaunMBennett <89603464+ShaunMBennett@users.noreply.github.com>
2022-11-06 16:12:32 -0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Team3BucHunt.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
