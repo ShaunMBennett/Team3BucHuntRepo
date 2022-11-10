ca8d4213bf3d7ceee69e3cabf23a6671d9e872c3
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2022-11-10 10:18:24 -0500

    Working on database, added code generator. Removed unnecessary (pre-made example) files.

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using team3.Data;
+
+namespace team3.Services;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    DbSet<Tasks> TaskList => Set<Tasks>();
+}
