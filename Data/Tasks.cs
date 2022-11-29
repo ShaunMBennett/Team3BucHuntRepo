0581309d10af6e568f8da3d7dac7fd2faebe0378
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2022-11-29 10:41:36 -0500

    Added functionality so that the application can communicate with the database

---
+++ Data/Tasks.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace team3.Data;
+
+/// <summary>
+/// Added by Dylan Shaffer.
+/// Purpose is to create a Task object so that
+/// the application can communicate with the DB.
+/// </summary>
+public class Tasks
+{
+    [Key]
+    public int TaskID { get; set; }
+    [Required]
+    public string Location { get; set; } = String.Empty;
+    [Required]
+    public string Question { get; set; } = String.Empty;
+    [Required]
+    public string Answer { get; set; } = String.Empty;
+}
---
+++ Services/TasksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using team3.Pages;
+
+namespace team3.Services;
+
+/// <summary>
+/// Added by Dylan Shaffer
+/// Purpose is to handle the logic for the Task list,
+/// at time of design it should just take tasks from DB
+/// and display them on razor page.
+/// </summary>
+public class TasksController : Controller
+{
+    private readonly ITaskRepo _taskRepo;
+
+    public TasksController(ITaskRepo taskRepo)
+    {
+        _taskRepo = taskRepo;
+    }
+    
+    public IActionResult TaskList()
+    {
+        return View(_taskRepo.ReadAll());
+    }
+}
